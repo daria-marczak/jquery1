3aced55cd73052dab06cd4aaeff34a9594bd4514
daria marczak <marczak.ria@gmail.com>
2017-11-11 22:31:35 +0100

    modified

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ $(function() {
 
 var span = $("span");
 span.each(function(index, element) {
-	$("span:even").css("color", "red");
+    $("span:even").css("color", "red");
 });
 
 var paragraphs = $("p");
@@ -13,9 +13,9 @@ var paragraphs = $("p");
 paragraphs.each(function(index, element) {
     var button = '<button class="btn" data-tmp="' + index + '">Click me</button>';
     $(element).append(button);
-    $(element).click(function() {
+    $(button).click(function() {
         alert($(this).attr("data-tmp"));
-    })
+    });
 });
 //
 // $("button").click(function() {
