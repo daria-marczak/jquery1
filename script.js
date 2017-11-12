75357709ac8531131f8eaff3a3d98796271c2519
daria marczak <marczak.ria@gmail.com>
2017-11-12 14:30:50 +0100

    modified

--- script.js
+++ script.js
@@ -12,9 +12,11 @@ var paragraphs = $("p");
 
 paragraphs.each(function(index, element) {
     var button = '<button class="btn" data-tmp="' + index + '">Click me</button>';
-    $(element).append(button);
-    $(button).click(function() {
-        alert($(this).attr("data-tmp"));
+
+    var $buttonClick = $(element).append(button);
+
+    $buttonClick.click(function() {
+        alert($(button).attr("data-tmp"));
     });
 });
 //
